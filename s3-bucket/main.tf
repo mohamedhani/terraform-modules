9e59837f2dd2bc66a9545b9ffc089cc4769fe563
Mohamed Hani <mohamedhani01014@gmail.com>
2022-02-17 04:01:25 +0200

    reformat terraform code

--- s3-bucket/main.tf
+++ s3-bucket/main.tf
@@ -2,12 +2,12 @@ data "aws_caller_identity" "current" {}
 
 resource "aws_s3_bucket" "main_bucket" {
 
-    bucket = "${data.aws_caller_identity.current.account_id}-${var.bucket_name}"
-    force_destroy = var.force_destroy
+  bucket        = "${data.aws_caller_identity.current.account_id}-${var.bucket_name}"
+  force_destroy = var.force_destroy
 }
 
 resource "aws_s3_bucket_acl" "main_bucket_acl" {
-  bucket =  aws_s3_bucket.main_bucket.id
+  bucket = aws_s3_bucket.main_bucket.id
   acl    = "private"
 }
 
@@ -15,28 +15,28 @@ resource "aws_s3_bucket_acl" "main_bucket_acl" {
 resource "aws_s3_bucket_versioning" "main_bucket_versioning" {
   bucket = aws_s3_bucket.main_bucket.id
   versioning_configuration {
-    status = var.has_versioning ? "Enabled" : "Suspended"
+    status     = var.has_versioning ? "Enabled" : "Suspended"
     mfa_delete = "Disabled"
   }
 }
 
 
 resource "aws_s3_bucket_lifecycle_configuration" "main_bucket_lifecycle" {
-  
+
 
   bucket = aws_s3_bucket.main_bucket.bucket
   rule {
-       id = "lifecycle rule"
-      expiration {
-          days = var.expiration_days
-      }
-     transition {
+    id = "lifecycle rule"
+    expiration {
+      days = var.expiration_days
+    }
+    transition {
       days          = var.transition_days
       storage_class = "STANDARD_IA"
     }
-     status = "Enabled"
+    status = "Enabled"
   }
-  
+
 }
 
 resource "aws_s3_bucket_lifecycle_configuration" "versioning_main_bucket_lifecycle" {
