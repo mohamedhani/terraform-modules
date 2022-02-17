204f3851ef57373c24a7dd7b44bd14d38229c9b4
Mohamed Hani <mohamedhani01014@gmail.com>
2022-02-18 00:58:56 +0200

    update of s3 bucket module

--- s3-bucket/main.tf
+++ s3-bucket/main.tf
@@ -15,50 +15,34 @@ resource "aws_s3_bucket_acl" "main_bucket_acl" {
 resource "aws_s3_bucket_versioning" "main_bucket_versioning" {
   bucket = aws_s3_bucket.main_bucket.id
   versioning_configuration {
-    status     = var.has_versioning ? "Enabled" : "Suspended"
+    status     = var.enable_versioning ? "Enabled" : "Suspended"
     mfa_delete = "Disabled"
   }
 }
 
 
-resource "aws_s3_bucket_lifecycle_configuration" "main_bucket_lifecycle" {
+resource "aws_s3_bucket_lifecycle_configuration" "main_bucket_lifecycle_policy" {
 
-
+ 
   bucket = aws_s3_bucket.main_bucket.bucket
   rule {
-    id = "lifecycle rule"
+    id = "s3 lifecycle policy"
     expiration {
       days = var.expiration_days
     }
-    transition {
-      days          = var.transition_days
-      storage_class = "STANDARD_IA"
-    }
-    status = "Enabled"
-  }
-
-}
-
-resource "aws_s3_bucket_lifecycle_configuration" "versioning_main_bucket_lifecycle" {
-  # Must have bucket versioning enabled first
-  depends_on = [aws_s3_bucket_versioning.main_bucket_versioning]
-
-  bucket = aws_s3_bucket.main_bucket.bucket
-
-  rule {
-    id = "versions lifecylce rule"
-
-
     noncurrent_version_expiration {
       noncurrent_days = var.expiration_days
+    }
+    transition  {
+      days = var.transition_days
+      storage_class   = "STANDARD_IA"
     }
 
     noncurrent_version_transition {
       noncurrent_days = var.transition_days
       storage_class   = "STANDARD_IA"
     }
-
-    status = var.has_versioning ? "Enabled" : "Disabled"
+    status =   "Enabled"
   }
 }
 
