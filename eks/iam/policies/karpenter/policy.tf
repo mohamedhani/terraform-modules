9e4056a310d5d4591e6922405ef069268faf230c
Mohamed Hani <mohamedhani01014@gmail.com>
2024-02-18 02:00:51 +0200

    fix issue

--- eks/iam/policies/karpenter/policy.tf
+++ eks/iam/policies/karpenter/policy.tf
@@ -2,7 +2,7 @@ data "aws_caller_identity" "current" {}
 data "aws_region" "current" {}
 data "aws_partition" "current" {}
 locals {
-  region     = data.aws_caller_identity.current.name
+  region     = data.aws_region.current.name
   partition  = data.aws_partition.current.partition
   account_id = data.aws_caller_identity.current.id
 }
