b245f03974ba7e7cd7eb17665b95f94d83b9ca86
Mohamed Hani <mohamedhani01014@gmail.com>
2024-02-23 11:23:01 +0200

    fix on public-subnet module

--- network/subnet/public/nat_gatways.tf
+++ network/subnet/public/nat_gatways.tf
@@ -1,5 +1,5 @@
 module "nat_gateway" {
-  for_each = toset(var.single_nat_gateway ? local.sorted_azs[0] : local.sorted_azs)
+  for_each = toset(var.single_nat_gateway ? [local.sorted_azs[0]] : local.sorted_azs)
 
   source      = "../../nat_gatway"
   name_prefix = "${var.vpc_name}-${each.value}"
